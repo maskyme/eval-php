ce7d68078e0a19b9c37aed1e351e0aebd68462ca
margot LADISLAS <margot.ladislas@apside-groupe.com>
2025-04-04 10:10:45 +0200

    header role

--- header.php
+++ header.php
@@ -1,33 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+session_start();
+$user = $_SESSION['user'];
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu</title>
-    <link rel="stylesheet" href="styles.css">
-    <script src="https://cdn.tailwindcss.com"></script>   
+if (isset($user['user_categ'])) {
+    $_SESSION['role'] = $user['user_categ'];
+} else {
+    $_SESSION['role'] = 0;
+}
 
-</head>
+$role = $_SESSION['role'];
+?>
 
-<body>
-    <header class="header flex  items-center bg-blue-600 text-white">
-        <div class="flex w-full max-w-[1600px] mx-auto justify-between items-center py-3 px-3">
-            <div class="logo">
-                <a href="#" class="text-2xl font-bold hidden sm:block">Garage Attens</a>
-                <a href="#" class="text-2xl font-bold block sm:hidden">GA</a>
-            </div>
-            <nav class="nav">
-                <ul class="nav-list flex gap-5 sm:gap-10">
-                    <?php
-                        echo '<li><a href="#employees" class="text-sm sm:text-xl font-bold hover:opacity-90">Employés</a></li>';
-                    ?>
-                    <li><a href="#interventions" class="text-sm sm:text-xl font-bold hover:opacity-90">Interventions</a></li>
-                    <li><a href="#account" class="text-sm sm:text-xl font-bold hover:opacity-90">Account</a></li>
-                </ul>
-            </nav>
+<header class="header flex  items-center bg-blue-600 text-white">
+    <div class="flex w-full max-w-[1600px] mx-auto justify-between items-center py-3 px-3">
+        <div class="logo">
+            <a href="#" class="text-2xl font-bold hidden sm:block">Garage Attens</a>
+            <a href="#" class="text-2xl font-bold block sm:hidden">GA</a>
         </div>
-    </header>
-</body>
+        <nav class="nav">
+            <ul class="nav-list flex gap-5 sm:gap-10">
+                <?php
+                if ($role == 1) {
+                    echo '<li><a href="#list_employe" class="text-sm sm:text-xl font-bold hover:opacity-90">Employés</a></li>';
+                    echo '<li><a href="#list_client" class="text-sm sm:text-xl font-bold hover:opacity-90">Employés</a></li>';
+                }
 
-</html>
+                ?>
+                <li><a href="#interventions" class="text-sm sm:text-xl font-bold hover:opacity-90">Interventions</a></li>
+                <li><a href="#account" class="text-sm sm:text-xl font-bold hover:opacity-90">Account</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -1,15 +1,18 @@
+
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-        echo "<pre>";
-        print_r($user);
-        echo "</pre>";
+        require_once './sign-in.php';
     ?>
+
 </body>
+
 </html>
