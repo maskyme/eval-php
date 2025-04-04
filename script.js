a36f36dd7a760c4e6814aaa48b759ac3dbd312ec
frndeva <evaferrand33@gmail.com>
2025-04-04 13:18:26 +0200

    t

--- script.js
+++ script.js
@@ -23,6 +23,15 @@ function editIntervention(
   interventionId,
   longDescription
 ) {
+  console.log({
+    id,
+    clientId,
+    startDate,
+    startHour,
+    employeeId,
+    interventionId,
+    longDescription,
+  });
   document.getElementById("edit-intervention-popup").classList.remove("hidden");
 
   document.getElementById("edit-id-intervention").value = id;
@@ -30,7 +39,7 @@ function editIntervention(
   document.getElementById("edit-startDate").value = startDate; // ✅ format "2025-04-04"
   document.getElementById("edit-startHour").value = startHour; // ✅ format "14:00"
   document.getElementById("edit-employe").value = employeeId;
-  document.getElementById("edit-intervention").value = interventionId;
+  document.getElementById("edit-intervention-name").value = interventionId;
   document.getElementById("edit-longDescription").value = longDescription;
 }
 
