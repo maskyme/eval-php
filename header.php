a254146e089586a871965ef8608c15a1590d7335
maskyme <maxymejarrierpro@gmail.com>
2025-04-04 09:00:16 +0200

    header responsive

--- header.php
+++ header.php
@@ -9,17 +9,18 @@
 </head>
 <body>
     <header class="header flex  items-center bg-blue-600 text-white">
-        <div class="flex w-full max-w-[1600px] mx-auto justify-between items-center py-3">
+        <div class="flex w-full max-w-[1600px] mx-auto justify-between items-center py-3 px-3">
             <div class="logo">
-                <a href="#" class="text-2xl font-bold">Garage Attens</a>
+                <a href="#" class="text-2xl font-bold hidden sm:block">Garage Attens</a>
+                <a href="#" class="text-2xl font-bold block sm:hidden">GA</a>
             </div>
             <nav class="nav">
-                <ul class="nav-list flex gap-10">
+                <ul class="nav-list flex gap-5 sm:gap-10">
                     <?php
-                        echo '<li><a href="#employees" class="text-xl font-bold hover:opacity-90">Employés</a></li>';
+                        echo '<li><a href="#employees" class="text-sm sm:text-xl font-bold hover:opacity-90">Employés</a></li>';
                     ?>
-                    <li><a href="#interventions" class="text-xl font-bold hover:opacity-90">Interventions</a></li>
-                    <li><a href="#account" class="text-xl font-bold hover:opacity-90">Account</a></li>
+                    <li><a href="#interventions" class="text-sm sm:text-xl font-bold hover:opacity-90">Interventions</a></li>
+                    <li><a href="#account" class="text-sm sm:text-xl font-bold hover:opacity-90">Account</a></li>
                 </ul>
             </nav>
         </div>
