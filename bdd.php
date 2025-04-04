852e397daf51e9668e3d326b45f27ab18b275d50
margot LADISLAS <margot.ladislas@apside-groupe.com>
2025-04-04 09:11:04 +0200

    bdd

--- bdd.php
+++ bdd.php
@@ -1,14 +1,13 @@
 <?php
-    $servername = "localhost";
-    $username = "Maxyme";
-    $password = "G4qr@7vT#";
-    $dbname = "garage";
-    
-    try {
-        $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    } 
-    catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
+$servername = "213.32.90.176";
+$username = "root";
+$password = "ekod2025";
+$dbname = "garage";
+
+try {
+    $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
--- header.php
+++ header.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Beautiful Header Menu</title>
+    <title>Menu</title>
     <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
     <header class="header">
         <div class="logo">
@@ -14,7 +16,9 @@
         <nav class="nav">
             <ul class="nav-list">
                 <?php
+                if ($user['category_id'] == 1) {
                     echo '<li><a href="#employees">Employés</a></li>';
+                }
                 ?>
                 <li><a href="#interventions">Interventions</a></li>
                 <li><a href="#account">Account</a></li>
@@ -22,4 +26,5 @@
         </nav>
     </header>
 </body>
+
 </html>
